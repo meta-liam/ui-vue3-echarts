dbb2cfa3220acaa9c549de70c2c7dab277e0d7c7
liam1902 <liam.jiang@makeblock.com>
2022-05-19 15:43:45 +0800

    feat: init.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Vue3-ECharts-demo
+
+实现ECharts例子。
+
+## 快速开始
+
+```shell
+npm run dev
+
+npm run build
+```
+
+## 效果
+
+![alt preview](./docs/h5.jpg)
+
+![alt preview](./docs/ios.jpg)
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- <HelloWorld msg="HelloWorld"></HelloWorld> -->
+  <LinkChannel msg="link"></LinkChannel>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import LinkChannel from './components/LinkChannel.vue'
+import { reactive, toRefs, computed, onBeforeMount, onMounted } from 'vue'
+
+export default {
+  name: 'App',
+  components: {
+    LinkChannel
+    //HelloWorld
+  },
+  setup() {
+  }
+
+}
+</script>
